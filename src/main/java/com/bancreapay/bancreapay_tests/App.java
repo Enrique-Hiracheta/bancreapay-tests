a5ba2f5d727443cf6b34667a66939c42367129ea
Hiracheta <josehiracheta@hotmail.com>
2026-03-02 13:54:32 -0600

    Initial test structure

---
+++ src/main/java/com/bancreapay/bancreapay_tests/App.java
@@ -0,0 +1,10 @@
+package com.bancreapay.bancreapay_tests;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/bancreapay/bancreapay_tests/LoginTest.java
@@ -0,0 +1,38 @@
+package com.bancreapay.bancreapay_tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import java.net.URL;
+
+public class LoginTest {
+
+    private WebDriver driver;
+    // Selenium Grid corre en el contenedor, desde Jenkins apunta al hub
+    private static final String GRID_URL = "http://selenium-hub:4444/wd/hub";
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setBrowserName("chrome");
+        driver = new RemoteWebDriver(new URL(GRID_URL), caps);
+    }
+
+    @Test
+    public void testLoginPageLoads() {
+        driver.get("https://tu-url-de-bancreapay.com/login");
+        Assert.assertTrue(driver.getTitle().contains("Login"), 
+            "El título de la página debe contener 'Login'");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
